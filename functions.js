3ca86d491a962eb566aabfd102ed41d13e10a37e
Rawle Juglal <rawledjuglal@gmail.com>
2023-03-24 17:06:39 -0500

    completed firebase project
    Completed and personalized firebase project

--- functions.js
+++ functions.js
@@ -2,17 +2,6 @@ export function clearInputField(element){
     element.value = ""
 }
 
-export function addItemToCart(element, item){
-    let newEl = document.createElement('li');
-    newEl.classList.add('list-item');
-    newEl.id = item[0];
-    newEl.textContent = item[1];
-    newEl.addEventListener('click',(event)=>{
-        console.log(event.target.id);
-    })
-    element.append(newEl)
-}
-
 export function clearShoppingListEl(element){
     element.innerHTML = ''
 }
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
-import { clearInputField, addItemToCart, clearShoppingListEl } from './functions';
+import { clearInputField, clearShoppingListEl } from './functions';
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
-import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 const appSettings = {
   databaseURL: `https://realtime-database-a7c40-default-rtdb.firebaseio.com/`
@@ -16,16 +16,35 @@ const AddToCartInput = document.getElementById('grocery-input');
 const shoppingListEl = document.getElementById('shopping-list');
 
 onValue(shoppingListInDB, (snapshot)=>{
-  let shoppingListArray = Object.entries(snapshot.val());
-  clearShoppingListEl(shoppingListEl)
-  shoppingListArray.map(item => {
-    addItemToCart(shoppingListEl, item)
-  })
+  if(snapshot.exists()){
+    let shoppingListArray = Object.entries(snapshot.val());
+    clearShoppingListEl(shoppingListEl)
+    shoppingListArray.map(item => {
+      addItemToCart(shoppingListEl, item)
+    })
+  } else {
+    shoppingListEl.innerHTML = `<p>No Items here...yet</p>`
+  }
+  
 })
+
+function addItemToCart(element, item){
+  let newEl = document.createElement('li');
+  newEl.classList.add('list-item');
+  let groceryItem = item[0];
+  newEl.textContent = item[1];
+  newEl.addEventListener('dblclick',(event)=>{
+    let exactLocationOfItemInBD = ref(database, `shoppingList/${groceryItem}` )
+  
+      remove(exactLocationOfItemInBD);
+  })
+  element.append(newEl)
+}
 
 AddToCartBTN.addEventListener('click', ()=> {
   let inputValue = AddToCartInput.value;
-  push(shoppingListInDB, inputValue);
+  if(inputValue){
+    push(shoppingListInDB, inputValue);
+  } 
   clearInputField(AddToCartInput);
-  // addItemToCart(inputValue);
 })
