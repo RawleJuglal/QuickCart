44cb57efd5aab367f263498ac4e300977966af9e
Rawle Juglal <rawledjuglal@gmail.com>
2023-03-23 21:21:17 -0500

    feature added
    call database to populate shopping list, new items can be added to database
    
    upon continue, removing item from database

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 import './style.css'
+import { clearInputField, addItemToCart, clearShoppingListEl } from './functions';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+
+const appSettings = {
+  databaseURL: `https://realtime-database-a7c40-default-rtdb.firebaseio.com/`
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList")
+
+const AddToCartBTN = document.getElementById('add-button');
+const AddToCartInput = document.getElementById('grocery-input');
+const shoppingListEl = document.getElementById('shopping-list');
+
+onValue(shoppingListInDB, (snapshot)=>{
+  let shoppingListArray = Object.entries(snapshot.val());
+  clearShoppingListEl(shoppingListEl)
+  shoppingListArray.map(item => {
+    addItemToCart(shoppingListEl, item)
+  })
+})
+
+AddToCartBTN.addEventListener('click', ()=> {
+  let inputValue = AddToCartInput.value;
+  push(shoppingListInDB, inputValue);
+  clearInputField(AddToCartInput);
+  // addItemToCart(inputValue);
+})
